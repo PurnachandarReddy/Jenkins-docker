04d8ada0f19acdc60e4f23e9b1916fcfde3dcc66
Purnachandar <kpcreddy21@gmail.com>
2024-02-12 15:13:29 +0530

    first commit

---
+++ src/main/java/com/example/JenkinsDockerIntegration/JenDockController.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsDockerIntegration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenDockController {
+	@GetMapping("/show")
+	public String showData() {
+		return "This application is running from Jenkins, First pushed to github and then Jenkins converted this as Image and pushed that to dockerhub";
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsDockerIntegration/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsDockerIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
